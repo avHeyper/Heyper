3b23f7e263549334a409d83932ae09bf5a6b48cd
Heyper <avheyper@gmail.com>
2016-11-18 22:59:34 +0100

    Add files via upload

--- src/Setup.java
+++ src/Setup.java
@@ -1,1 +1,17 @@
+import graph.Wizard;
 
+
+public class Setup {
+	
+	public static void main(String args[]){
+		
+		// Create a new window for the instalation.
+		Wizard setup = new Wizard();
+		setup.setVisible(true);
+		
+		
+		
+		
+		
+	}
+}

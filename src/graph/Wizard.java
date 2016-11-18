604e1a3c053930a6ddc84fc147a5ca27a72ec7e5
Heyper <avheyper@gmail.com>
2016-11-18 22:56:53 +0100

    Add files via upload

--- src/graph/Wizard.java
+++ src/graph/Wizard.java
@@ -1,1 +1,23 @@
+package graph;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Wizard extends JFrame{
+	/**
+	 * Create the setup window.
+	 */
+	public Wizard() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}

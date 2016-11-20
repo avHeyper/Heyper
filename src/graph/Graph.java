8d3e9d511c25425aedcd04d4137fdda250b3af74
Heyper <avheyper@gmail.com>
2016-11-20 16:54:20 +0100

    Update Graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -52,6 +52,7 @@ public class Graph {
 	 * Description :Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		/*creacion del frame*/
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.getContentPane().setFont(new Font("UnPilgi", Font.PLAIN, 12));
@@ -65,7 +66,7 @@ public class Graph {
 		frame.getContentPane().setLayout(null);
 		frame.setUndecorated(true);
 		frame.setOpacity((float) 0.80);
-		
+		/*boton de ajustes*/
 		JButton setting = new JButton("");
 		setting.setEnabled(false);
 		setting.setBorder(null);
@@ -73,7 +74,7 @@ public class Graph {
 		setting.setBounds(287, 8, 39, 44);
 		setting.setIcon(new ImageIcon(Graph.class.getResource("/graph/res/images/ajustes.png")));
 		frame.getContentPane().add(setting);
-		
+		/*boton para añadir funciones*/
 		JButton addfunbutton = new JButton("");
 		addfunbutton.setBackground(UIManager.getColor("Slider.tickColor"));
 		addfunbutton.setForeground(UIManager.getColor("Slider.tickColor"));
@@ -81,7 +82,7 @@ public class Graph {
 		addfunbutton.setIcon(new ImageIcon(Graph.class.getResource("/graph/res/images/addoption1.png")));
 		addfunbutton.setBounds(245, 12, 34, 34);	
 		frame.getContentPane().add(addfunbutton);
-		
+		/*label donde se mostraran los comandos asociados*/
 		JLabel label = new JLabel("Comandos --opciones");
 		label.setFont(label.getFont().deriveFont(label.getFont().getStyle() | Font.BOLD | Font.ITALIC));
 		label.setForeground(new Color(192, 192, 192));
@@ -89,7 +90,7 @@ public class Graph {
 		label.setOpaque(true);
 		label.setBounds(47, 73, 243, 34);
 		frame.getContentPane().add(label);
-		
+		/*label provisional que se sustituira por el logo*/
 		JLabel lblHeyper = new JLabel("HEYPER");
 		lblHeyper.setForeground(new Color(138, 43, 226));
 		lblHeyper.setBackground(UIManager.getColor("Slider.tickColor"));

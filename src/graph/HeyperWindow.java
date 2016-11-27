e3ec580ec8164928fa6f2a9c4a523d7a8cee7e6f
Heyper <avheyper@gmail.com>
2016-11-27 14:08:14 +0100

    Update HeyperWindow.java

--- src/graph/HeyperWindow.java
+++ src/graph/HeyperWindow.java
@@ -13,13 +13,12 @@ import Heyper.Configuration;
  * @since 26 November 2016
  */
 public class HeyperWindow extends JFrame{
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Create the application.
+	 * Description: Constructor of the frame.
+	 * @params config of type Configuration. 
 	 */
 	public HeyperWindow(Configuration config) {
 		setResizable(false);
@@ -30,16 +29,6 @@ public class HeyperWindow extends JFrame{
 		setOpacity((float) 0.80);
 		HeyperPanel panel = new HeyperPanel();
 		setContentPane(panel);
-			/*frame.getContentPane().setFont(new Font("UnPilgi", Font.PLAIN, 12));
-			frame.getContentPane().setEnabled(false);
-			frame.getContentPane().setForeground(new Color(255, 20, 147));
-			frame.getContentPane().setBackground(UIManager.getColor("Slider.tickColor"));
-			frame.getContentPane().setLayout(null);
-			frame.getContentPane().add(HeyperPanel.setting);
-			frame.getContentPane().add(HeyperPanel.addfunbutton);
-			frame.getContentPane().add(HeyperPanel.label);
-			frame.getContentPane().add(HeyperPanel.lblHeyper);
-			frame.getContentPane().add(HeyperPanel.exit);*/
 			
 	}
 
